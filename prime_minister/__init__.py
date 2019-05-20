e7403e584ba8d15ce7b1aa3c4386f9e8fa18d0b0
D Tim Cummings <tim@triptera.com.au>
2019-05-20 17:26:24 +1000

    task 2 including code for demo 2
    Also updated Bob Hawke's date of death

--- prime_minister/__init__.py
+++ prime_minister/__init__.py
@@ -29,43 +29,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # Utility functions
-
-    # EXTRA: function to calculate age
-    def age(birthday, age_day=date.today()):
-        """Returns person's age at age_day given their birthday
-        Checks if they have already had their birthday in final year
-        """
-        birthday_in_age_year = date(year=age_day.year, month=birthday.month, day=birthday.day)
-        if birthday_in_age_year > age_day:
-            return age_day.year - birthday.year - 1
-        else:
-            return age_day.year - birthday.year
-
-    # Routes to web pages
+    # EXTRA: Add routes to web pages
 
     @app.route('/')
     def index():
         return render_template('index.html', page_title="Beginner's Python Web Application")
-
-    # EXTRA: Add function to view a single person
-    @app.route('/person/<id_person>')
-    def view_person(id_person):
-        if not id_person:
-            abort(404)
-        pm_db = db.get_db()
-        person = pm_db.execute('select * from tbl_person where id=?', (id_person,)).fetchone()
-        if not person:
-            abort(404)
-        if person['date_death'] and person['date_birth']:
-            person = dict(person)  # convert to dict as sqlite3.Row does not support item assignment
-            person['age_death'] = age(person['date_birth'], person['date_death'])
-        sql = """select m1.*, m2.date_start as date_end
-            from tbl_ministry m1 left join tbl_ministry m2 on m1.id_next=m2.id
-            where m1.id_person=?
-            order by m1.date_start asc"""
-        ministries = pm_db.execute(sql, (id_person,)).fetchall()
-        return render_template('view_person.html', person=person, ministries=ministries, page_title="View person")
 
     @app.route('/persons/')
     def view_persons():
@@ -76,7 +44,7 @@ def create_app(test_config=None):
         ministry = "Prime Minister"
         page_title = ministry + 's'
         pm_db = db.get_db()
-        sql = """select p.id, p.vc_common_name, p.vc_surname, p.date_birth, p.vc_birth_place, p.date_death 
+        sql = """select distinct p.id, p.vc_common_name, p.vc_surname, p.date_birth, p.vc_birth_place, p.date_death 
                 from tbl_person p inner join tbl_ministry m on p.id=m.id_person 
                 where m.vc_ministry=?
                 order by p.vc_surname asc, p.vc_common_name asc"""
@@ -85,14 +53,7 @@ def create_app(test_config=None):
 
     # Filters
 
-    # EXTRA: date_format filter
-    @app.template_filter('date_format')
-    def date_format_filter(value, date_format="%d-%b-%Y"):
-        try:
-            return value.strftime(date_format)
-        except AttributeError:
-            return value
-
+    from . import db
     db.init_app(app)
 
     return app
